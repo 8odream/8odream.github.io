9f4d1f07346e6e8aacd7ced21dc37d3e4996d90d
8odream <87055981+8odream@users.noreply.github.com>
2022-04-20 17:11:21 +0800

    greatly change!

---
+++ doc/passage/ps.js
@@ -0,0 +1,10 @@
+function pass(pas){
+    var x
+    if(pas=='7758258'){
+        x=('https://8odream.github.io/doc/passage/人.html')}
+    else{
+        x=('密码错误')
+    }
+    }
+    
+document.getElementsById('1').innerHTML=x

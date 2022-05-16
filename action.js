45cb077d0bdf298aba4a20b5c0d14d6cc3d5b4eb
8odream <87055981+8odream@users.noreply.github.com>
2022-05-16 14:27:32 +0800

    build

--- action.js
+++ action.js
@@ -3,17 +3,20 @@ function count(){
     count+=1;
     remove_layer1()
     remove_layer2()
+var fun2=document.getElementById(layer2);
+var fun1=document.getElementById(layer1);
+var fun0=document.getElementById(layer0)
 }
 function remove_layer2(){
     if(count==1){
-        var fun1=document.getElementById(layer2);
-        fun1.style.cssText='color:rgba(255, 255, 255, 0)';
+        fun2.style.display='none';
+        fun1.style.display='block';
     }
 }
 function remove_layer1(){
     if(count==2){
-        var fun2=document.getElementById(layer1);
-        fun2.style.cssText='color:rgba(255, 255, 255, 0)';
+        fun1.style.display='none';
+        fun0.style.display='block';
 
     }
 }

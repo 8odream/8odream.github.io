a64c7bf7bbeeb4882769045f52dea344599e6334
8odream <87055981+8odream@users.noreply.github.com>
2022-05-22 12:01:43 +0800

    build

--- action.js
+++ action.js
@@ -1,20 +1,20 @@
-var count=0;
+var num=0;
 var fun2=document.getElementById('layer2');
 var fun1=document.getElementById('layer1');
 var fun0=document.getElementById('layer0');
 function count(){
-    count+=1;
+    num+=1;
     remove_layer1();
     remove_layer2();
 }
 function remove_layer2(){
-    if(count==1){
+    if(num==1){
         fun2.style.display='none';
         fun1.style.display='block';
     }
 }
 function remove_layer1(){
-    if(count==2){
+    if(num==2){
         fun1.style.display='none';
         fun0.style.display='block';
     }

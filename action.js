7fb7352cc00554f96f33e16ae9e6e3d445564672
8odream <87055981+8odream@users.noreply.github.com>
2022-05-25 17:12:25 +0800

    build

--- action.js
+++ action.js
@@ -1,16 +1,18 @@
-var num=0;
+window.cout_num=0
 function count_remove(){
-    num=num+1;
-    if(num==1){
+    cout_num=cout_num+1;
+    if(cout_num==1)
+    {
         document.getElementById('layer2').style.display='none';
         document.getElementById('layer1').style.display='block';
-    }
-    else if(num==2){
+        }
+    else if(cout_num==2)
+    {
         document.getElementById('layer1').style.display='none';
         document.getElementById('layer0').style.display='block';
-    }
+        }
     else{
-        num=0;
+        cout_num=0;
         document.getElementById('layer2').style.display='block';
         document.getElementById('layer0').style.display='none';
     }

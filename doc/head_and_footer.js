bf089c42d3a78e8f70c5e0e178418f1ae59a95a0
8odream <87055981+8odream@users.noreply.github.com>
2022-06-01 17:09:47 +0800

    build

--- doc/head_and_footer.js
+++ doc/head_and_footer.js
@@ -1,8 +1,4 @@
-function to_head(){
-    var head_id=document.getElementById('tohead');
-    head_id.innerHTML='<div class="lead"><li><a href="/index.html">主页</a></li><li><a href="/doc/tech/tech_index.html">科技</a></li><li><a href="/doc/culture/culture_index.html">文学</a></li><li><a href="/doc/talks/talks_index.html">杂谈</a></li><li class="key"><a href="/class/class_index.html"><img src="/钥匙.png" height="25" width="25"></a></li></div>';
-}
-function to_end(){
-    var end_id=document.getElementById('toend');
-    end_id.innerHTML='<div>联系我：<br><table><tr><td>QQ：348280600</td><td>github:8odream</td></tr></table></div>';
-}
+var head_id=document.getElementById('tohead');
+head_id.innerHTML='<div class="lead"><li><a href="/index.html">主页</a></li><li><a href="/doc/tech/tech_index.html">科技</a></li><li><a href="/doc/culture/culture_index.html">文学</a></li><li><a href="/doc/talks/talks_index.html">杂谈</a></li><li class="key"><a href="/class/class_index.html"><img src="/钥匙.png" height="25" width="25"></a></li></div>';
+var end_id=document.getElementById('toend');
+end_id.innerHTML='<div>联系我：<br><table><tr><td>QQ：348280600</td><td>github:8odream</td></tr></table></div>';

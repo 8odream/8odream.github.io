25786673c8bd95c180c1a0002e97410b02a6b256
8odream <87055981+8odream@users.noreply.github.com>
2022-06-01 16:56:07 +0800

    build

---
+++ doc/head_and_footer.js
@@ -0,0 +1,4 @@
+function tohead(){
+    var head_id=document.getElementById('head_');
+    head_id.innerHTML='<div class="lead"><li><a href="/index.html">主页</a></li><li><a href="/doc/tech/tech_index.html">科技</a></li><li><a href="/doc/culture/culture_index.html">文学</a></li><li><a href="/doc/talks/talks_index.html">杂谈</a></li><li class="key"><a href="/class/class_index.html"><img src="/钥匙.png" height="25" width="25"></a></li></div>';
+}

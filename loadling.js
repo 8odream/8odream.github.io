cf41b26c24285642ecf1c2b4908ee0063033f310
8odream <87055981+8odream@users.noreply.github.com>
2022-11-12 14:02:21 +0800

    amination change

---
+++ loadling.js
@@ -0,0 +1,6 @@
+if (window.onload==false){
+    document.getElementById('body').style.display='none';
+}
+else{document.getElementById('loading').style.display='none';
+    document.getElementById('body').style.display='solid';
+}
